76b90f74d17bf17c944bdd973f0a022f8daa0252
mariashiferaw <66911924+mariashiferaw@users.noreply.github.com>
2021-12-06 08:27:45 -0500

    Add files via upload

---
+++ Paragraphs.java
@@ -0,0 +1,17 @@
+public class Paragraphs{
+	private String para;
+	private Integer counter;
+	
+	public Paragraphs(String paragraphs, Integer count)
+	{
+		para = paragraphs;
+		counter = count;
+	}
+	public String getpara() {
+		return para;	
+	}
+	
+	public Integer getcount() {
+		return counter;
+	}
+}
